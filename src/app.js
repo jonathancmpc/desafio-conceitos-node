cb2ab43ce7400b30fc52f248a70fc958b0492ed9
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-06-17 10:34:33 -0300

    List, Create, Update and Delete

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,22 +10,100 @@ app.use(cors());
 
 const repositories = [];
 
+/* Middlewares */
+function validateProjectID(request, response, next){
+  const { id } = request.params;
+
+  /* Verifica se o ID é válido */
+  if (!isUuid(id)){
+    return response.status(400).json({error: 'Invalid repository ID'});
+  }
+
+  /* Prossegue para a próxima Middleware caso o id seja válido */
+  return next();
+}
+
+/* Chamada de Middleware's para as rotas */
+app.use('/repositories/:id', validateProjectID);
+
+
+/* List/Queries */
 app.get("/repositories", (request, response) => {
-  // TODO
+  const { techs } = request.query;
+
+  /* Verificando se o filtro é válido */
+  const results = techs 
+    ? repositories.filter(repository => repository.techs.includes(techs))
+    :repositories
+
+  /* Retorna resultado da consulta ao cliente */
+  return response.json(results);
 });
 
+/* Create */
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  /* Montando array com dados passados pelo cliente */
+  const repository = { id:uuid(), title, url, techs };
+
+  /* Inserindo os dados no banco de dados */
+  repositories.push(repository);
+
+  /* Resposta para o cliente */
+  return response.json(repository);
+
 });
 
+/* Update */
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const { title, url, techs } = request.body;
+
+  /* Percorrendo banco de dados e verificando se existe o parâmetro ID */
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+  /* Retornando mensagem ao cliente caso não exista o id */
+  if (repositoryIndex < 0){
+    return response.status(400).json({error: "Repository not found"});
+  }
+
+  /* Montando novo array com alterações */
+  const repository = {
+    id,
+    title,
+    url,
+    techs
+  }
+
+  /* Incluindo alterações no banco de dados */
+  repositories[repositoryIndex] = repository;
+
+  /* Retornando mensagem ao cliente */
+  return response.json(repository);
 });
 
+/* Delete */
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  
+  /* Percorrendo banco de dados e verificando se existe o parâmetro ID */
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+  /* Retornando mensagem ao cliente caso não exista o id */
+  if (repositoryIndex < 0){
+    return response.status(400).json({error: "Repository not found"});
+  }
+
+  /* Deletando repositório */
+  repositories.splice(repositoryIndex, 1);
+
+  /* Retornando mensagem em branco para o cliente */
+  return response.status(204).send();
 });
 
+/* Create Like's */
 app.post("/repositories/:id/like", (request, response) => {
   // TODO
 });
